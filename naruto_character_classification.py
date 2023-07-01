7f7e93f53e8f58e4fe25a11abfb44e18365acc64
guptg <57366940+guptg@users.noreply.github.com>
2023-07-01 15:03:18 -0500

    Add files via upload

---
+++ naruto_character_classification.py
@@ -0,0 +1,18 @@
+# Python 
+import argparse
+import sys 
+
+# Local
+
+
+def main(argv):
+    
+    # Parse command line arguments 
+    parser = argparse.ArgumentParser(description="Recognizing characters from Naruto Face Dataset from kaggle: \
+                                                  https://www.kaggle.com/neetuk/naruto-face-dataset/")
+    
+
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
